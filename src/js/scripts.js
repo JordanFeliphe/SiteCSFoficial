1f521f94b02b6f933f60e7d2cd77484965bfd19a
Jordan Feliphe <165801343+JordanFeliphe@users.noreply.github.com>
2024-07-24 09:09:05 -0300

    ultimo commit,fim.

---
+++ src/js/scripts.js
@@ -0,0 +1,1 @@
+window.addEventListener("scroll",function(){document.querySelector(".scrollTop").classList.toggle("active",window.scrollY>450)});function backTop(){window.scrollTo({top:0,behavior:"smooth"})}
